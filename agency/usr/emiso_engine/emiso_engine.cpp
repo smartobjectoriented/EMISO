7b3a993f221877fa0637ad096b3e3b32de929ccf
Jean-Pierre Miceli <jean-pierre.miceli@heig-vd.ch>
2023-09-01 09:52:24 +0200

    [emiso engine] Enable HTTPS/TLS support
    Signed-off-by: Jean-Pierre Miceli <jean-pierre.miceli@heig-vd.ch>

--- agency/usr/emiso_engine/emiso_engine.cpp
+++ agency/usr/emiso_engine/emiso_engine.cpp
@@ -4,13 +4,16 @@ using namespace httpserver;
 
 class hello_world_resource : public http_resource {
  public:
-     const std::shared_ptr<http_response> render(const http_request&) {
+    const std::shared_ptr<http_response> render(const http_request&) {
          return std::shared_ptr<http_response>(new string_response("Hello, World!"));
      }
 };
 
 int main() {
-    webserver ws = create_webserver(8080);
+    webserver ws = create_webserver(8080)
+        .use_ssl()
+        .https_mem_key("key.pem")
+        .https_mem_cert("cert.pem");
 
     hello_world_resource hwr;
     ws.register_resource("/hello", &hwr);
