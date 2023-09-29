573881bcc4e9e34de4f32b6b8cfd6a68068bffe7
Jean-Pierre Miceli <jean-pierre.miceli@heig-vd.ch>
2023-09-29 13:51:05 +0200

    [webserver] Add 'Get system information' API
    In this version, the values are hardcoded !
    
    Signed-off-by: Jean-Pierre Miceli <jean-pierre.miceli@heig-vd.ch>

--- agency/usr/emiso_engine/webserver/system/system.hpp
+++ agency/usr/emiso_engine/webserver/system/system.hpp
@@ -41,12 +41,85 @@ namespace emiso {
         public:
 
             const std::shared_ptr<httpserver::http_response> render_GET(const httpserver::http_request &req) {
-
                 std::string payload_str = "";
                 Json::Value payload_json;
 
-                payload_json["ID"]         = "2ff11d3f-c408-493e-b170-908919f68044";
+                payload_json["ID"]         = "2ff11d3f-c408-493e-b170-908919f68044"; // WARNING - should be unique
                 payload_json["Containers"] = 2;
+                payload_json["ContainersRunning"] = 1;
+                payload_json["ContainersPaused"] = 0;
+                payload_json["ContainersStopped"] = 0;
+                payload_json["Images"] = 3;
+
+                // WARNING - hardcoded values got from Docker answer of JMI PC
+                payload_json["Driver"] = "overlay2";
+                payload_json["DriverStatus"][0].append("Backing Filesystem");
+                payload_json["DriverStatus"][0].append("extfs");
+                payload_json["DriverStatus"][1].append("Supports d_type");
+                payload_json["DriverStatus"][1].append("true");
+                payload_json["DriverStatus"][2].append("Using metacopy");
+                payload_json["DriverStatus"][2].append("false");
+                payload_json["DriverStatus"][3].append("Native Overlay Diff");
+                payload_json["DriverStatus"][3].append("true");
+                payload_json["DriverStatus"][4].append("userxattr");
+                payload_json["DriverStatus"][4].append("false");
+
+                payload_json["DockerRootDir"] = "/var/lib/docker"; // default value on Linux
+                payload_json["Plugins"] = Json::objectValue;
+
+                // Set these values to false as not supported/handled by emiso
+                payload_json["MemoryLimit"] = "false";
+                payload_json["SwapLimit"] = "false";
+                payload_json["CpuCfsPeriod"] = "false";
+                payload_json["CpuCfsQuota"] = "false";
+                payload_json["CPUShares"] = "false";
+                payload_json["CPUSet"] = "false";
+                payload_json["PidsLimit"] = "false";
+                payload_json["OomKillDisable"] = "false";
+                payload_json["IPv4Forwarding"] = "false";
+                payload_json["BridgeNfIptables"] = "false";
+                payload_json["BridgeNfIp6tables"] = "false";
+                payload_json["Debug"] = "false";
+
+                payload_json["SystemTime"] = "2017-08-08T20:28:29.06202363Z"; // GET REAL/current system time
+                payload_json["LoggingDriver"] = "json-file"; // WARNING - hardcoded values got from Docker answer of JMI PC
+                payload_json["CgroupDriver"] = "none";
+                payload_json["CgroupVersion"] = 1; // Default docker value
+                payload_json["NEventsListener"] = 0;
+
+                // Get these information for system !!!!!!
+                payload_json["KernelVersion"] = "5.10";
+                payload_json["OperatingSystem"] = "Buildroot 2022.02.5";
+                payload_json["OSVersion"]    = "-gd5d03f1ce-dirty";
+                payload_json["OSType"] = "linux";
+                payload_json["Architecture"] = "aarch64";
+
+                payload_json["NCPU"] = 1;
+                payload_json["MemTotal"] = 536870912; // To check !
+                payload_json["IndexServerAddress"] = "https://index.docker.io/v1/";
+                payload_json["RegistryConfig"] = Json::objectValue;
+                payload_json["GenericResources"] = Json::arrayValue;
+
+                payload_json["HttpProxy"] =  "";    // use  'HTTP_PROXY' env variable
+                payload_json["HttpsProxy"] =  "";  // use 'HTTPS_PROXY' env variable
+                payload_json["NoProxy"] =  "";   // use 'NO_PROXY' env variable
+                payload_json["Name"] =  "agency";   // Host name
+                payload_json["Labels"] = Json::arrayValue;
+                payload_json["ExperimentalBuild"] = config::experimental;
+                payload_json["ServerVersion"] = "24.0.5";
+                payload_json["ServerVersion"] = Json::objectValue;
+                payload_json["DefaultRuntime"] = "";
+
+                payload_json["Swarm"]["NodeID"] = "";
+                payload_json["Swarm"]["NodeAddr"] = "";
+                payload_json["Swarm"]["LocalNodeState"] = "inactive";
+                payload_json["Swarm"]["ControlAvailable"] = "false";
+                payload_json["Swarm"]["Error"] = "";
+                payload_json["Swarm"]["RemoteManagers"] = Json::objectValue;
+
+                payload_json["LiveRestoreEnabled"] = false;
+                payload_json["Isolation"] = "VT";  // TODO - platform specific
+                payload_json["SecurityOptions"] = Json::arrayValue;
 
                 Json::StreamWriterBuilder builder;
                 payload_str = Json::writeString(builder, payload_json);
